be905471f4f2d8c77b37a5bb71b70f5ea7982666
Nelson Marro <nelsonmarro99@gmail.com>
2024-05-27 10:58:23 -0500

    Intial api skeleton

---
+++ mednacursos/app/01-main-demo/page.js
@@ -0,0 +1,12 @@
+import HomePageLayout from "./(main-demo)";
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <>
+      <HomePageLayout />
+    </>
+  );
+};
+
+export default HomePage;
